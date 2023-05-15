3c384fbd6ecb275178427a006577c795579678e6
wygrajr <wycliffe.graftonjr@gmail.com>
2023-05-15 11:55:23 -0400

    Added local storage capability to 12AM block

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+//Add Date at top of Scheduler
 $(document).ready(function() {
   var dateNow=dayjs();
 
 $("#currentDay").text(dateNow.format("dddd, MMMM D"));
 });
 
+//Scheduler recognizes what time of day it is
 var hourNow=dayjs().hour();
 
 $(".time-block").each(function(){
@@ -19,3 +21,13 @@ $(".time-block").each(function(){
     $(this).addClass("present")
   }
 });
+
+//local storage of data inputed by user
+$(".saveBtn").on("click",function(){
+  var userInput=$(this).siblings(".description").val();
+  var timeBlock=$(this).parent().attr("id");
+
+  localStorage.setItem(timeBlock, JSON.stringify(userInput))
+});
+
+$("#hour-0 .description").val(JSON.parse(localStorage.getItem("hour-0")));
