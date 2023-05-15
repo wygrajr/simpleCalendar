12328d7ab68f5414220755563b161513f4a8ff2b
wygrajr <wycliffe.graftonjr@gmail.com>
2023-05-15 11:40:17 -0400

    Modified HTML IDs within time-block class. Added in javascript calendar function to recognize what time of day it is.

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ $(document).ready(function() {
 $("#currentDay").text(dateNow.format("dddd, MMMM D"));
 });
 
-$(function () {
-  
+var hourNow=dayjs().hour();
+
+$(".time-block").each(function(){
+  $(this).removeClass("past present future");
+
+  var currentHour=parseInt($(this).attr("id").split('-')[1]);//<-- splits the ID at - and converts it to an array.
+
+  if (hourNow>currentHour){
+    $(this).addClass("past");
+  } else if (hourNow<currentHour){
+    $(this).addClass("future");
+  } else {
+    $(this).addClass("present")
+  }
 });
